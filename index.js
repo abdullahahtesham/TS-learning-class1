d1053ea39651d932957e632ac4b0f7c1eba11eff
abdullahahtesham <146721363+abdullahahtesham@users.noreply.github.com>
2024-03-24 15:31:00 +0500

    updated

--- index.js
+++ index.js
@@ -1,5 +1,2 @@
-let greed = "hello world";
-console.log(greed);
-export {};
-let userName = "Abdullah"
-console.log(userName)
+console.log("Hello World"); //maybe the double or single dot
+console.log("Hello World"); // semicolon will be added in the js but in the ts we have no need of the semicolon compiler will automatically added semicolon to the javascript file
